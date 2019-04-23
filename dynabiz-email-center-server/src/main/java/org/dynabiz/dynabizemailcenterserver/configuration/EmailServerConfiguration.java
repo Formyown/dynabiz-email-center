1febdc8a12d0c5ba6548b7ecf3fee986494f9cad
Deyu Heng <1119242459@qq.com>
2019-04-23 23:56:27 +1000

    :construction: Progress update

--- dynabiz-email-center-server/src/main/java/org/dynabiz/dynabizemailcenterserver/configuration/EmailServerConfiguration.java
+++ dynabiz-email-center-server/src/main/java/org/dynabiz/dynabizemailcenterserver/configuration/EmailServerConfiguration.java
@@ -1,7 +1,16 @@
 package org.dynabiz.dynabizemailcenterserver.configuration;
 
+import org.dynabiz.dynabizemailcenterserver.controller.MailMessageController;
+import org.dynabiz.dynabizemailcenterserver.controller.MailTemplateTestController;
+import org.dynabiz.dynabizemailcenterserver.service.MailService;
+import org.dynabiz.dynabizemailcenterserver.service.MailTemplateService;
+import org.dynabiz.dynabizemailcenterserver.service.MailTemplateTestService;
+import org.dynabiz.dynabizemailcenterserver.vos.entity.MailTemplate;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({MailService.class, MailTemplateService.class, MailTemplateTestService.class,
+        MailMessageController.class, MailTemplate.class, MailTemplateTestController.class})
 public class EmailServerConfiguration {
 }
--- dynabiz-email-center-server/src/main/java/org/dynabiz/dynabizemailcenterserver/configuration/EnableMailCenterServer.java
+++ dynabiz-email-center-server/src/main/java/org/dynabiz/dynabizemailcenterserver/configuration/EnableMailCenterServer.java
@@ -3,7 +3,13 @@ package org.dynabiz.dynabizemailcenterserver.configuration;
 import org.dynabiz.dynabizemailcenterserver.support.mail.MailSendingHandler;
 import org.springframework.context.annotation.Import;
 
-@Import({MailSendingHandler.class, MessageQueueAutoConfiguration.class,
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import({EmailServerConfiguration.class,
+        MailSendingHandler.class, MessageQueueAutoConfiguration.class,
         TemplateEngineAutoConfiguration.class, RepositoryConfiguration.class})
-public class EnableMailCenterServer {
+public @interface EnableMailCenterServer {
 }
